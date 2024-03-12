c896cc73176c0096200ac8592c3e50db9d201999
nmcoulibaly <ndeyembarcoulibaly@gmail.com>
2024-03-12 23:59:29 +0100

    added routes

---
+++ routes/UserRoutes.js
@@ -0,0 +1,12 @@
+const UserController = require('../controllers/UserController');
+const routerUser = require('express').Router();
+
+routerUser.get('/users', UserController.getUsers);
+
+routerUser.get('/users/:id', UserController.getUserById);
+
+routerUser.post('/user/login', UserController.loginUser);
+
+routerUser.post('/user/register', UserController.registerUser);
+
+routerUser.put('/put/user/:id', UserController.putUser);
